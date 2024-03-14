a712859e7dd55ffab24d3374c3050e2f13133f4b
SALEEJA S <136804198+saleeja@users.noreply.github.com>
2024-03-14 19:43:54 +0530

    Update urls.py

--- my_project/my_project/urls.py
+++ my_project/my_project/urls.py
@@ -3,6 +3,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('app/', include('employees.urls')),
+    path('', include('employees.urls')),
 
 ]
