5c638be38c0a1f21d8455260a7065ab460080032
SALEEJA S <136804198+saleeja@users.noreply.github.com>
2024-03-14 20:15:09 +0530

    Update views.py

--- my_project/employees/views.py
+++ my_project/employees/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from .models import Department, Employee
-from django.shortcuts import render, get_object_or_404
 from django.views import View
 from django.views.generic import ListView, DetailView
 
